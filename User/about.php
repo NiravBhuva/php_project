ab443e78f8bbf4d089ea45eda9f6cfffcdd4fe3e
NiravBhuva <niravpatel5499@gmail.com>
2022-06-27 01:06:50 +0530

    Add: ContactUs & AboutUs

---
+++ User/about.php
@@ -0,0 +1,67 @@
+		
+		
+<?php
+	include "Header.php";
+?>		
+<!-- about -->
+<div class="privacy about">
+	<h3>About Us</h3>
+	<br><br>
+	<strong style="font-size: 0.93em;">Our customers come first, always.</strong>
+	<p class="animi">Grow your business from the customized visiting cards, choose from the variety of visiting card templates and visiting card designs which we offer you to choose from. Online business cards that we make are made with such finesse that impresses everyone holding your card</p>
+	<p class="animi">Custom business cards are perfect for building your brand. With the help of our business card maker, you’ll be able to design the perfect business card with your logo displayed front and center. Choose from hundreds of pre-made templates from dozens of different industries. Our full-color printing and high-quality paper is guaranteed to make a great impression.</p>
+	<br>
+	<strong style="font-size: 0.93em;">Our customers count on us, and we deliver. </strong>
+	<p class="animi">We never pull a fast one on you, and we deliver on time, and often sooner. There are no hidden surprises, charges or special conditions. You will know exactly where your project is — up to the very minute it is delivered.</p>
+</div>
+</div>
+	<div class="clearfix"></div>
+</div>
+<!-- //banner -->
+
+<!-- testimonials -->
+<div class="testimonials">
+		<div class="container">
+			<h3>Testimonials</h3>
+				<div class="w3_testimonials_grids">
+					<div class="wmuSlider example1 animated wow slideInUp" data-wow-delay=".5s">
+						<div class="wmuSliderWrapper">
+							<article style="position: absolute; width: 100%; opacity: 0;"> 
+								<div class="banner-wrap">
+									<div class="col-md-6 w3_testimonials_grid">
+										<p><i class="fa fa-quote-right" aria-hidden="true"></i>The customer service is very responsive. They thoroughly guide you through the entire process of getting quality. You get to connect with a rich pool of activated, interested, and highly motivated designers. I felt taken care of and would highly recommend using it.</p>
+										<h4>Andrew Smith</h4>
+									</div>
+									<div class="col-md-6 w3_testimonials_grid">
+										<p><i class="fa fa-quote-right" aria-hidden="true"></i>I explained to them what I wanted and all these different factors I was trying to balance and they interpreted it perfectly. The logo contest ended up being so helpful because I got to see different people's interpretations of what my goals were.</p>
+										<h4>Thomson Richard</h4>
+									</div>
+									<div class="clearfix"> </div>
+								</div>
+							</article>
+							<article style="position: absolute; width: 100%; opacity: 0;"> 
+								<div class="banner-wrap">
+									<div class="col-md-6 w3_testimonials_grid">
+										<p><i class="fa fa-quote-right" aria-hidden="true"></i>The pricing was very reasonable. They were tailored to the client’s requirements and very user friendly. A lot of great professional designers were available to present ideas, the quality of designs was great and everything was done in a very timely manner.</p>
+										<h4>Crisp Kale</h4>
+									</div>
+									<div class="col-md-6 w3_testimonials_grid">
+										<p><i class="fa fa-quote-right" aria-hidden="true"></i>The experience has been great. In fact, every day we're surprised by the kind of initiatives that the website takes to get the best designers to work on your designing needs. It's a great platform to get the best designs, at an affordable price</p>
+										<h4>John Paul</h4>
+									</div>
+									<div class="clearfix"> </div>
+								</div>
+							</article>
+						</div>
+					</div>
+					<script src="js/jquery.wmuSlider.js"></script> 
+					<script>
+						$('.example1').wmuSlider();         
+					</script> 
+				</div>
+		</div>
+	</div>
+<!-- //testimonials -->
+<?php
+	include "Footer.php";
+?>
